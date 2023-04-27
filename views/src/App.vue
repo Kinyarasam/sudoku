0e371566bc939a82eba1e905cc16b4055ed3861e
kinyarasam <skinyara.30@gmail.com>
2023-04-27 10:39:49 +0300

    Components

--- views/src/App.vue
+++ views/src/App.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
-    <h1>Sudoku</h1>
+    <div class="flex justify-center border-b hover:shadow py-4">
+      <header class="underline font-bold text-3xl uppercase">
+        <h1>Sudoku</h1>
+      </header>
+    </div>
+    <div class="bg-gray-300 p-4 m-10 flex justify-center items-center">
+      <Sudoku />
+    </div>
+    <footer></footer>
   </div>
 </template>
 
 <script>
+import Sudoku from './components/Sudoku.vue';
+
 export default {
   name: 'App',
-  components: {}
+  components: {
+    Sudoku
+  }
 }
 </script>
 
---
+++ views/src/components/Sudoku.vue
@@ -0,0 +1,25 @@
+<template>
+  <div @click="gameLoad()">
+    Sudoku
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SudokuGameView',
+  data() {
+    return {
+      //
+    }
+  },
+  methods: {
+    gameLoad(){
+      console.log('Started')
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
